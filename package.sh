09bd2a52aded53874f243d1f791630bed1070b66
stevenstetzler <steven.stetzler@gmail.com>
2021-05-21 11:46:07 -0700

    update charts and repository?

---
+++ package.sh
@@ -0,0 +1,10 @@
+for chart in aws-hub genesis-dev growth-hub sssc-hub; do
+    helm dependency update $chart
+    res=$(helm package $chart)
+    path=$(echo $res | awk '{split($0, a, " "); print a[length(a)]}')
+    mv $path docs/$(basename $path)
+done
+
+cd docs
+helm2 repo index .
+cp index.yaml _data/index.yaml
